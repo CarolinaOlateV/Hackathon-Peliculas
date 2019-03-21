c74036ebc398950848642e9b47a8cb3ef5a413cc
Relun <claudia.araya.ro@gmail.com>
2019-03-21 12:08:56 -0300

    agregando input y boton para buscar las peliculas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+
 const getData = () => {
    
-    fetch ('http://www.omdbapi.com/?i=tt3896198&apikey=d8fb7dad&s=starwars')// le damos la ruta que queremos seguir, si es una api publicada le damos la url que queremos que siga
+    fetch ('http://www.omdbapi.com/?i=tt3896198&apikey=d8fb7dad&s=amor')// le damos la ruta que queremos seguir, si es una api publicada le damos la url que queremos que siga
         .then(Response => {    // fetch trabaja con el comando .then
             return Response.json()// va a leer los archivos json
         })
@@ -11,3 +12,8 @@ const getData = () => {
         .catch(error => error)
     }
       window.onload=getData();
+        
+         
+
+
+        
