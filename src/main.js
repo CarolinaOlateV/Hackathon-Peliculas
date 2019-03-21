8cb07147d53e662d3a731d269624e54ca2410a4d
Relun <claudia.araya.ro@gmail.com>
2019-03-21 16:16:47 -0300

    agregando lista de peliculas y que la muestre en la pagina

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 
-const getData = () => {
-   
-    fetch ('http://www.omdbapi.com/?i=tt3896198&apikey=d8fb7dad&s=amor')// le damos la ruta que queremos seguir, si es una api publicada le damos la url que queremos que siga
-        .then(Response => {    // fetch trabaja con el comando .then
-            return Response.json()// va a leer los archivos json
-        })
-        .then(data =>{
-            console.log(data)
-            seeData(data);// aqui llamo la funcion que declaramos mas abajo
-        })
-        .catch(error => error)
-    }
-      window.onload=getData();
-        
-         
+/*const getData = () => {
 
+   fetch ('http://www.omdbapi.com/?i=tt3896198&apikey=d8fb7dad&t=amor')// le damos la ruta que queremos seguir, si es una api publicada le damos la url que queremos que siga
+       .then(Response => {    // fetch trabaja con el comando .then
+           return Response.json()// va a leer los archivos json
+       })
+       .then(data =>{
 
-        
+        console.log(data)
+        //for(let i=0;i<data.length;i++){
+          //  if(data[i].Year.includes(2011)){
+            //}
+
+        //}
+           // aqui llamo la funcion que declaramos mas abajo
+       })
+       .catch(error => error)
+   }
+     window.onload=getData();*/
